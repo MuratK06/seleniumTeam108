59dc4aa979df8b95c13b1fb9730c8d4a0dccbdfe
Murat <muratkorkmaz8306@gmail.com>
2023-03-09 20:49:06 +0100

    practice

--- src/test/java/practice/AyseHoca/Q2.java
+++ src/test/java/practice/AyseHoca/Q2.java
@@ -1,4 +1,4 @@
-package practice;
+package practice.AyseHoca;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
--- src/test/java/practice/AyseHoca/Q2_Aysehoca.java
+++ src/test/java/practice/AyseHoca/Q2_Aysehoca.java
@@ -1,4 +1,4 @@
-package practice;
+package practice.AyseHoca;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
